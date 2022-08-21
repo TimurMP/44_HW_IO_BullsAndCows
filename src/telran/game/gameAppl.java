7919e7f56056b1a1a0116bd3562b93efef6c6b35
TimurMP <titimurmamyshev@gmail.commur>
2022-08-21 23:39:43 +0300

    numberGenerator

--- src/telran/game/gameAppl.java
+++ src/telran/game/gameAppl.java
@@ -1,2 +1,31 @@
-package telran.game;public class gameAppl {
+package telran.game;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class gameAppl {
+
+    public static void main(String[] args) {
+        numberGenerator();
+
+    }
+
+
+
+    public static ArrayList<Integer> numberGenerator () {
+        ArrayList <Integer> numbers = new ArrayList<>();
+        for(int i = 1; i < 9; i++)
+        {
+            numbers.add(i);
+        }
+        Collections.shuffle(numbers);
+        numbers.subList(4, numbers.size()).clear();
+        for (Integer number : numbers) {
+            System.out.print(number);
+
+        }
+        return numbers;
+    }
+
+
 }
